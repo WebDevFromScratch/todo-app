802b7d811421a619cf20d9d4d2fdc12c16ed180e
Piotr Klosinski <mybettingstuff@gmail.com>
2014-07-14 17:59:16 +0200

    fixed the user view, so it only shows tasks associated with the user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,6 +11,7 @@ class TasksController < ApplicationController
   def create
     @user = User.find(params[:user_id])
     @task = Task.new(task_params)
+    @task.user_id = params[:user_id]
 
     if @task.save
       flash[:notice] = "Your task was created!"
@@ -23,6 +24,6 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:name, :description)
+    params.require(:task).permit(:name, :description, :user_id)
   end
 end
