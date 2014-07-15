0da49d1935e88917c3c6f615a9ef6d6393551d27
Piotr Klosinski <mybettingstuff@gmail.com>
2014-07-15 12:10:13 +0200

    finished the login/logout funtionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(username: params[:username])
-    if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
-      redirect_to user_path(current_user) #current_user
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user) #current_user
     else
       flash[:error] = "There is something wrong with your Username and/or Password"
       redirect_to login_path
@@ -15,6 +15,8 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_path
+
+    flash[:notice] = "You have successfully logged out"
+    redirect_to login_path #change this to sth that makes sense later
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+  before_action :require_same_user, only: [:show]
+
   def new
     @user = User.new
   end
@@ -7,22 +10,32 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      current_user = @user
       flash[:notice] = "You have successfully registered"
       redirect_to user_path(@user)
     else
-      binding.pry
       render :new
     end
   end
 
   def show
-    @user = User.find(params[:id])
     @tasks = Task.where(user_id: @user.id)
   end
-end
 
-private
+  private
 
-def user_params
-  params.require(:user).permit(:username, :password, :password_confirmation)
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:error] = "You're not allowed to do that"
+      redirect_to login_path #change later
+    end
+  end
 end
